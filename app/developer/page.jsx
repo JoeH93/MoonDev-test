b7f91103fd18252c91f18cfa1d366e770508cf9e
joehachache <joephachache@gmail.com>
2025-04-30 15:01:08 +0300

    Fixing the import for DeveloperForm in it's main page

--- app/developer/page.jsx
+++ app/developer/page.jsx
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import DeveloperForm from './developerForm';
+import DeveloperForm from './DeveloperForm';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { redirect } from "next/navigation";
